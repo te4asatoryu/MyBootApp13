121af22686564c7b8c2c05f3f1b163a9d0e220b1
佐藤 隆太 <180598@jc-21.jp>
2021-05-27 13:39:05 +0900

    first commit

---
+++ src/main/java/jp/te4a/spring/boot/myapp6/BookBean.java
@@ -0,0 +1,11 @@
+package jp.te4a.spring.boot.myapp6;
+
+@Data
+@AllArgsConstructor
+public class BookBean  {
+	Integer id ;
+	String title;
+	String writter;
+	String publisher;
+	 Integer price;
+}
