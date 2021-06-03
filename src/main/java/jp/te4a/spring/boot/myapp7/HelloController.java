d143e2fd7603bc179d9e710c64add70de4a26c99
te4asatoryu <te3a180598@gmail.com>
2021-06-03 14:36:26 +0900

    kadai7

--- src/main/java/jp/te4a/spring/boot/myapp7/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp7/HelloController.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.boot.myapp6;
+package jp.te4a.spring.boot.myapp7;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,32 +9,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-
-public class HelloController {
-    
+public class HelloController {  
     @Autowired
     BookService bookService;
 
-    @RequestMapping("/")
+    @RequestMapping("books/list")
     public String index(Model model) {
         model.addAttribute("msg", "this is setting message");
-        return "index";
+        return "books/list";
     }
 
-    @RequestMapping(value="/post", method=RequestMethod.POST)
+    @RequestMapping(value="books/list", method=RequestMethod.POST)
     public ModelAndView postForm(@RequestParam("id") String id, 
    @RequestParam("title") String title,@RequestParam("writter") String writter, 
    @RequestParam("publisher") String publisher,@RequestParam("price") String price) {
-        ModelAndView mv = new ModelAndView("index");
+        ModelAndView mv = new ModelAndView("books/list");
         bookService.save(new BookBean(Integer.valueOf(id), title, writter, publisher, Integer.valueOf(price)));
-       StringBuffer buff = new StringBuffer();
-       buff.append("<HR>");
-       for(BookBean bean:bookService.findAll()) {
-           buff.append("ID:" + bean.getId() + "<BR>" + "タイトル:" + bean.getTitle() + 
-          "<BR>"+ "著者:" + bean.getWritter() + "<BR>" + "出版社:" + bean.getPublisher() + 
-          "<BR>"+ "価格:" + bean.getPrice() + "<BR><HR>");
-       }
-       mv.addObject("msg", buff.toString());
+       mv.addObject("books", bookService.findAll());
        return mv;
    }
 
