8b2fcf4daac78991ee6aa6510e48aa6076f94c39
te4asatoryu <te3a180598@gmail.com>
2021-06-15 09:33:34 +0900

    mybootapp8

--- src/test/java/jp/te4a/spring/boot/myapp8/MyBootApp8ApplicationTests.java
+++ src/test/java/jp/te4a/spring/boot/myapp8/MyBootApp8ApplicationTests.java
@@ -1,10 +1,10 @@
-package jp.te4a.spring.boot.myapp7;
+package jp.te4a.spring.boot.myapp8;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyBootApp7ApplicationTests {
+class MyBootApp8ApplicationTests {
 
 	@Test
 	void contextLoads() {
